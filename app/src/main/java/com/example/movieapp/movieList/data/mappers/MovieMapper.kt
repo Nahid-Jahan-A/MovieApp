80bab45213041ddfbd0d59c3cdcbd6104088a19b
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-03-02 18:11:21 +0600

    repo codes and mappers completed

--- app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
@@ -1,11 +1,41 @@
 package com.example.movieapp.movieList.data.mappers
 
 import com.example.movieapp.movieList.data.local.movie.MovieEntity
+import com.example.movieapp.movieList.data.remote.respond.MovieDto
 import com.example.movieapp.movieList.domain.model.Movie
+
+
+fun MovieDto.toMovieEntity(
+    category: String
+): MovieEntity {
+    return MovieEntity(
+        adult = adult ?: false,
+        backdrop_path = backdrop_path ?: "",
+        original_language = original_language ?: "",
+        overview = overview ?: "",
+        poster_path = poster_path ?: "",
+        release_date = release_date ?: "",
+        title = title ?: "",
+        vote_average = vote_average ?: 0.0,
+        popularity = popularity ?: 0.0,
+        vote_count = vote_count ?: 0,
+        id = id ?: -1,
+        original_title = original_title ?: "",
+        video = video ?: false,
+
+        category = category,
+
+        genre_ids = try {
+            genre_ids?.joinToString(",") ?: "-1,-2"
+        } catch (e: Exception) {
+            "-1,-2"
+        }
+    )
+}
 
 fun MovieEntity.toMovie(
     category: String
-) : Movie {
+): Movie {
     return Movie(
         backdrop_path = backdrop_path,
         original_title = original_title,
@@ -22,8 +52,13 @@ fun MovieEntity.toMovie(
         adult = adult,
 
         category = category,
+
         genre_ids = try {
-            genre_ids.spliterator().trySplit()
+            genre_ids.split(",").map {
+                it.toInt()
+            }
+        } catch (e: Exception) {
+            listOf(-1, -2)
         }
     )
 }
--- app/src/main/java/com/example/movieapp/movieList/data/repository/MovieListRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/repository/MovieListRepositoryImpl.kt
@@ -1,19 +1,25 @@
 package com.example.movieapp.movieList.data.repository
 
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import com.example.movieapp.movieList.data.local.movie.MovieDatabase
+import com.example.movieapp.movieList.data.mappers.toMovie
+import com.example.movieapp.movieList.data.mappers.toMovieEntity
 import com.example.movieapp.movieList.data.remote.MovieApi
 import com.example.movieapp.movieList.domain.model.Movie
 import com.example.movieapp.movieList.domain.repository.MovieListRepository
 import com.example.movieapp.movieList.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.io.IOException
 import javax.inject.Inject
 
 class MovieListRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
     private val movieDatabase: MovieDatabase
-)
-    : MovieListRepository{
+) : MovieListRepository {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override suspend fun getMovieList(
         forceFetchFromRemote: Boolean,
         category: String,
@@ -24,18 +30,68 @@ class MovieListRepositoryImpl @Inject constructor(
 
             val localMovieList = movieDatabase.movieDao.getMovieByCategory(category)
 
-            val shouldLoadLocal = localMovieList.isEmpty() && !forceFetchFromRemote
+            val shouldLoadLocal = localMovieList.isNotEmpty() && !forceFetchFromRemote
 
-            if(shouldLoadLocal) {
+            if (shouldLoadLocal) {
                 emit(Resource.Success(
-                    data = localMovieList
+                    data = localMovieList.map { movieEntity ->
+                        movieEntity.toMovie(category)
+                    }
                 ))
+                emit(Resource.Loading(false))
+                return@flow
             }
+
+            val movieListFromApi = try {
+                movieApi.getMovieList(category, page)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            }
+
+            val movieEntities = movieListFromApi.results.let {
+                it.map { movieDto ->
+                    movieDto.toMovieEntity(category)
+                }
+            }
+
+            movieDatabase.movieDao.upsertMovieList(movieEntities)
+
+            emit(Resource.Success(
+                movieEntities.map { it.toMovie(category) }
+            ))
+            emit(Resource.Loading(false))
+
         }
     }
 
     override suspend fun getMovie(id: Int): Flow<Resource<Movie>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(Resource.Loading(true))
+
+            val movieEntity = movieDatabase.movieDao.getMovieById(id)
+
+            if (movieEntity != null) {
+                emit(Resource.Success(movieEntity.toMovie(movieEntity.category)))
+
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            emit(Resource.Error("Error no such movie"))
+
+            emit(Resource.Loading(false))
+
+        }
     }
 
 }
