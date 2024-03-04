94051c13eb59b8998eed3bdff541f7248e262736
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-03-04 21:02:38 +0600

    hilt module created

---
+++ app/src/main/java/com/example/movieapp/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.movieapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+}
