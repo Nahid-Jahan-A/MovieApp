dcb5f29d77a27a6a2bac1604f9e40aa68a051e0f
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-03-05 21:46:54 +0600

    Module code added

--- app/src/main/java/com/example/movieapp/di/AppModule.kt
+++ app/src/main/java/com/example/movieapp/di/AppModule.kt
@@ -1,10 +1,19 @@
 package com.example.movieapp.di
 
+import androidx.room.Room
+import com.example.movieapp.App
+import com.example.movieapp.movieList.data.local.movie.MovieDatabase
+import com.example.movieapp.movieList.data.remote.MovieApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,4 +25,25 @@ object AppModule {
     private val client: OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(interceptor)
         .build()
+
+    @Singleton
+    @Provides
+    fun providesMoviesApi() : MovieApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(MovieApi.BASE_URL)
+            .client(client)
+            .build()
+            .create(MovieApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesMovieDatabase(app: App): MovieDatabase {
+        return Room.databaseBuilder(
+            app,
+            MovieDatabase::class.java,
+            name = "moviedb.db"
+        ).build()
+    }
 }
